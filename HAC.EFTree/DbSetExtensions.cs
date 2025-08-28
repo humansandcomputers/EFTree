e3acd0b3bb9e90d69df8141941ecae42479fe493
Nima Ghomri <nima.ghomri@gmail.com>
2025-08-28 14:09:12 +0330

    Working MinLeft and MaxRight methods

--- HAC.EFTree/DbSetExtensions.cs
+++ HAC.EFTree/DbSetExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using HAC.EFTree.Abstractions;
 using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
 namespace HAC.EFTree;
 
@@ -10,21 +11,21 @@ public static class DbSetExtensions
     public static void AddAfter<T>(this DbSet<T> set, T node, T? sibling = default)
         where T : Node
     {
-        var start = (sibling?.Right ?? set.MaxRight()) + 1;
+        var start = (sibling?.Right ?? set.MaxRight() ?? -1) + 1;
         set.Add(node, start);
     }
 
     public static void AddBefore<T>(this DbSet<T> set, T node, T? sibling = default)
         where T : Node
     {
-        var start = sibling?.Left ?? (set.MinLeft() + 2);
+        var start = sibling?.Left ?? set.MinLeft() ?? 0;
         set.Add(node, start);
     }
 
     public static void AddChild<T>(this DbSet<T> set, T node, T? parent = default)
         where T : Node
     {
-        var start = parent?.Right ?? (set.MaxRight() + 1);
+        var start = parent?.Right ?? ((set.MaxRight() ?? -1) + 1);
         set.Add(node, start);
     }
 
@@ -38,20 +39,50 @@ public static class DbSetExtensions
         set.Add(node);
     }
 
-    static long MaxRight<T>(this DbSet<T> set)
+    static long? Extremum<T>(this DbSet<T> set, Func<T?, long?> selector, Expression<Func<T?, long?>> expression, bool min)
+            where T : Node
+    {
+        var db = set.DefaultIfEmpty();
+        var local = set.Local.DefaultIfEmpty();
+
+        var dbValue = min ? db.Min(expression) : db.Max(expression);
+        var localValue = min ? local.Min(selector) : set.Max(selector);
+
+        if (dbValue is not null && localValue is not null)
+            return Math.Max(dbValue.Value, localValue.Value);
+        if (dbValue is not null)
+            return dbValue;
+        else if (localValue is not null)
+            return localValue;
+        return null;
+    }
+
+    static long? MaxRight<T>(this DbSet<T> set)
             where T : Node
     {
         var localMax = set.Local.DefaultIfEmpty().Max(x => x?.Right);
-        var dbMax = set.DefaultIfEmpty().Max(x => x == null ? null : x.Right);
-        return new[] { virtualNode.Right, localMax, dbMax }.OfType<long>().Max();
+        var dbMax = set.DefaultIfEmpty().Max(x => x is null ? null : x.Right);
+        if (localMax is { } lm && dbMax is { } dm)
+            return Math.Max(lm, dm);
+        if (localMax is null)
+            return dbMax;
+        if (dbMax is null)
+            return localMax;
+        return null;
     }
 
-    static long MinLeft<T>(this DbSet<T> set)
+    static long? MinLeft<T>(this DbSet<T> set)
             where T : Node
     {
         var localMin = set.Local.DefaultIfEmpty().Min(x => x?.Left);
         var dbMin = set.DefaultIfEmpty().Min(x => x == null ? null : x.Left);
-        return new[] { virtualNode.Left, localMin, dbMin }.OfType<long>().Min();
+        if (localMin is { } lm && dbMin is { } dm)
+            return Math.Max(lm, dm);
+        if (localMin is null)
+            return dbMin;
+        if (dbMin is null)
+            return localMin;
+        return null;
     }
 
     static void Shift<T>(this DbSet<T> set, long start, long offset)
@@ -65,6 +96,6 @@ public static class DbSetExtensions
         foreach (var x in rights)
             x.Right += offset;
     }
-    
+
     class VirtualNode : Node;
 }
