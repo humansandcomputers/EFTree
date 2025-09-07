3b0d3fd5bf8e4150486a33d96bde1fe5a1178735
Nima Ghomri <nima.ghomri@gmail.com>
2025-09-07 10:39:19 +0330

    Change both complex tests

--- HAC.EFTree.Tests/MovingTests.cs
+++ HAC.EFTree.Tests/MovingTests.cs
@@ -88,7 +88,7 @@ public class MovingTests(ITestOutputHelper output) : IDisposable
     /// \- E       	\- E
     /// </summary>
     [Fact]
-    public async Task Move_LeftToRightFromParentToAnotherParentWithGapBeforeAfterAndBetween_ValidHierarchy()
+    public async Task Move_LeftToRightComplex_ValidHierarchy()
     {
         // Arrange
         using (var context = factory.CreateDbContext())
@@ -145,31 +145,40 @@ public class MovingTests(ITestOutputHelper output) : IDisposable
 
 
     /// <summary>
-    /// |- A      => |- A       
-    /// |- B         |- B       
-    /// |- C         |  \- X    
-    /// |- D         |     \- X1
-    /// |  \- X      |- C       
-    /// |     \- X1  |- D       
-    /// \- E         \- E       
+    /// |- A       	|- A       
+    /// |- B       	|- B       
+    /// |  \- B1   	|  |- B1   
+    /// |- C       	|  \- X    
+    /// |- D       	|     \- X1
+    /// |  |- D1   	|- C       
+    /// |  |- D2   	|- D       
+    /// |  \- X    	|  |- D1   
+    /// |     \- X1	|  \- D2   
+    /// \- E       	\- E
     /// </summary>
     [Fact]
-    public async Task Move_RightToLeftFromParentToAnotherParentWithGapBeforeAfterAndBetween_ValidHierarchy()
+    public async Task Move_RightToLeftComplex_ValidHierarchy()
     {
         // Arrange
         using (var context = factory.CreateDbContext())
         {
             MockNode.Create(out var A);
             MockNode.Create(out var B);
+            MockNode.Create(out var B1);
             MockNode.Create(out var C);
             MockNode.Create(out var D);
+            MockNode.Create(out var D1);
+            MockNode.Create(out var D2);
             MockNode.Create(out var E);
             MockNode.Create(out var X);
             MockNode.Create(out var X1);
             context.Nodes.AddChild(A);
             context.Nodes.AddChild(B);
+            context.Nodes.AddChild(B1, B);
             context.Nodes.AddChild(C);
             context.Nodes.AddChild(D);
+            context.Nodes.AddChild(D1, D);
+            context.Nodes.AddChild(D2, D);
             context.Nodes.AddChild(X, D);
             context.Nodes.AddChild(X1, X);
             context.Nodes.AddChild(E);
@@ -188,13 +197,18 @@ public class MovingTests(ITestOutputHelper output) : IDisposable
         {
             TreeAssert.Single(context.Nodes, out var A);
             TreeAssert.Single(context.Nodes, out var B);
+            TreeAssert.Single(context.Nodes, out var B1);
             TreeAssert.Single(context.Nodes, out var C);
             TreeAssert.Single(context.Nodes, out var D);
+            TreeAssert.Single(context.Nodes, out var D1);
+            TreeAssert.Single(context.Nodes, out var D2);
             TreeAssert.Single(context.Nodes, out var E);
             TreeAssert.Single(context.Nodes, out var X);
             TreeAssert.Single(context.Nodes, out var X1);
             TreeAssert.Siblings(A, B, C, D, E);
+            TreeAssert.Siblings(B1, X);
             TreeAssert.Lineage(B, X, X1);
+            TreeAssert.Siblings(B1, X);
         }
     }
 
